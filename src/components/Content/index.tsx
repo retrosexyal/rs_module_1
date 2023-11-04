19878cd8a3bc01585aa4cb9e71e8d59026628de5
Artem Meldziuk <retrosexyal@gmail.com>
2023-11-04 14:20:45 +0300

    init module02

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -22,7 +22,7 @@ export class Content extends React.Component {
         const { data } = await FetchData.getSearch();
         this.context.handleData(data.results);
         this.context.handleLoading(false);
-        console.log(data);
+        console.log(data as any);
       }
     };
     fetchData();
